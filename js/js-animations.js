085e9d1451d27d1a342a9c42b1034bcb435b1383
Kaio <kaiolazaro36@hotmai.com>
2019-04-15 16:37:22 -0300

    HOC_DEV upload do site

---
+++ js/js-animations.js
@@ -0,0 +1,19 @@
+var speed = 300;
+
+$(".navbar-shop").hover(function() {
+	$(".navbar-shop ul li a").css('display', 'block');	
+	$(".navbar-shop ul li a").animate({
+		opacity: "1"
+	},speed, function() {
+	});
+}, function() {
+
+	$(".navbar-shop ul li a").animate({
+		opacity: "0"
+	},speed, function() {
+		var aux = setTimeout(function() {
+			$(".navbar-shop ul li a").css('display', 'none');	
+		}, speed);
+	});
+});
+
